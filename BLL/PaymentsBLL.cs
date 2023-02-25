09bb053d3843ada2225f5de39af19de5a57f7636
Levid <jassontejada@gmail.com>
2023-02-25 18:54:00 -0400

    Adding BLL

---
+++ BLL/PaymentsBLL.cs
@@ -0,0 +1,49 @@
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+public class PaymentsBLL
+{
+    private Context _context;
+
+    public PaymentsBLL(Context context)
+    {
+        _context = context;
+    }
+
+    public bool Exist(int paymentId)
+    {
+        return _context.Payments.Any(payment => payment.PaymentId == paymentId);
+    }
+    public bool Insert(Payments payment)
+    {
+        _context.Payments.Add(payment);
+        return _context.SaveChanges() > 0;
+    }
+    public bool Modify(Payments payment)
+    {
+        _context.Entry(payment).State = EntityState.Modified;
+        return _context.SaveChanges() > 0;
+    }
+    public bool Save(Payments payment)
+    {
+        if(!Exist(payment.PaymentId))
+            return Insert(payment);
+        else
+            return Modify(payment);
+    }
+    public bool Delete(Payments payment)
+    {
+        _context.Entry(payment).State = EntityState.Deleted;
+        return _context.SaveChanges() > 0;
+    }
+    public Payments? Search(int PaymentId)
+    {
+        Payments Payments = new Payments();
+        return Payments = _context.Payments.Include(payment => payment.PaymentDetails).Where(payment => payment.PaymentId == PaymentId).SingleOrDefault();
+        
+    }
+    public List<Payments> GetList(Expression<Func<Payments, bool>> criterion)
+    {
+        return _context.Payments.AsNoTracking().Where(criterion).ToList();
+    }
+}
